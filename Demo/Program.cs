38cac07855702afc97fa6d3674bc413c540181d0
Jan Bejvl <janbejvl@live.com>
2021-04-20 11:39:34 +0200

    update

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using Logic.Graphql.Generated;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace Demo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            serviceCollection
+                .AddBridgeClient()
+                .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://r69u3amow6.execute-api.eu-central-1.amazonaws.com"));
+            IServiceProvider services = serviceCollection.BuildServiceProvider();
+            IBridgeClient client = services.GetRequiredService<IBridgeClient>();
+
+            var result = await client.GetDetail.ExecuteAsync("Njz3jaQpG6", Currency.Czk);
         }
     }
 }
